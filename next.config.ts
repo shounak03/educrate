90f700231635fdffbc8d4a8601fe770e2793a105
shounak03 <shounakmis.3@gmail.com>
2024-11-22 21:15:56 +0530

    added cloudinary domain

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    images: {
+        domains: ['res.cloudinary.com'],
+        remotePatterns: [
+            {
+              protocol: 'https',
+              hostname: '**', // This allows all https image URLs
+            }
+          ]
+    }
 };
 
 export default nextConfig;
