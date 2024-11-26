d4cb6f6d79fec27c9c668060d18d7f33edaed970
shounak03 <shounakmis.3@gmail.com>
2024-11-26 20:12:47 +0530

    course database updated

--- models/courseContent.model.ts
+++ models/courseContent.model.ts
@@ -1,18 +1,60 @@
-import mongoose,{ Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const courseContentSchema = new Schema({
+const courseContentSchema = new Schema(
+  {
     course: {
-        type: Schema.Types.ObjectId,
-        ref: 'Course',
-        required: true
+      type: Schema.Types.ObjectId,
+      ref: "Course",
+      required: true,
+      unique: true
     },
-    Lectures: [{
-        type: String,
-        required: true
-    }],
-    docx:[{
-        type: String,
-    }],
-});
+    modules: [
+      {
+        moduleTitle: {
+          type: String,
+          required: true,
+        },
+        moduleDescription: {
+          type: String,
+        },
+        lectures: [
+          {
+            title: {
+              type: String,
+              required: true,
+            },
+            content: {
+              type: String,
+              required: true,
+            },
+            videoUrl: {
+              type: String,
+            },
+            duration: {
+              type: Number, // duration in minutes
+            },
+            resources: [
+              {
+                name: {
+                  type: String,
+                },
+                fileUrl: {
+                  type: String,
+                },
+                type: {
+                  type: String,
+                  enum: ['pdf', 'document', 'spreadsheet', 'presentation', 'other']
+                }
+              }
+            ],
+          }
+        ]
+      }
+    ]
+  },
+  { 
+    timestamps: true 
+  }
+);
 
-export const CourseContent = mongoose.models.CourseContent || mongoose.model('CourseContent', courseContentSchema);
+export const CourseContent = mongoose.models.CourseContent || mongoose.model("CourseContent", courseContentSchema);
--- models/courses.model.ts
+++ models/courses.model.ts
@@ -1,71 +1,70 @@
 import mongoose, { Schema } from "mongoose";
 
-const courseSchema = new Schema({
+const courseSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true
-
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     creator: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
     price: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     duration: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
-    keyword: [{
+    keyword: [
+      {
         type: String,
-    }],
-    thumbnail: {
-        type: String,
-        required: true
-    },
-    enrolledStudents: [{
-        type: Schema.Types.ObjectId,
-        ref: "User"
-    }],
-    reviews: {
-        type: Array,
-        default: []
-    },
-    contents: [
-        {
-            videoLecture: {
-                type: String,
-                required: true
-            },
-            documentation: {
-                type: String
-            },
-        }
+      },
     ],
-    ratings: [{
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    enrolledStudents: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    reviews: {
+      type: Array,
+      default: [],
+    },
+    courseContent: {
+      type: Schema.Types.ObjectId,
+      ref: "CourseContent"
+    },
+    ratings: [
+      {
         student: {
-            type: Schema.Types.ObjectId,
-            ref: 'User'
+          type: Schema.Types.ObjectId,
+          ref: "User",
         },
         rating: {
-            type: Number,
-            min: 1,
-            max: 5
+          type: Number,
+          min: 1,
+          max: 5,
         },
         date: {
-            type: Date,
-            default: Date.now
-        }
-    }],
-}, { timestamps: true })
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-export const Course = mongoose.models.Course || mongoose.model('Course', courseSchema);
-
-// module.exports.Course
+export const Course = mongoose.models.Course || mongoose.model("Course", courseSchema);
