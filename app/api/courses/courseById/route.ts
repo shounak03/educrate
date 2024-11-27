649ffadb7be7d1cd0373148f0be488ad2a17981d
shounak03 <shounakmis.3@gmail.com>
2024-11-27 20:44:37 +0530

    updated to fetch course content

--- app/api/courses/courseById/route.ts
+++ app/api/courses/courseById/route.ts
@@ -1,25 +1,16 @@
-
-import { connectDB } from "@/lib/dbConnect";
+import { CourseContent } from "@/models/courseContent.model";
 import { Course } from "@/models/courses.model";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async(req:NextRequest,res:NextResponse) => {
-    
     try {
-        await connectDB();
         const courseId = req.nextUrl.searchParams.get("courseId");
-        // console.log(courseId);
+
+        console.log("id - ", courseId);
         
-        if(!courseId)
-            return NextResponse.json({success:false,message:"Course ID is required"},{status:400})
-        
-        console.log("test 1");
-        
-        const course = await Course.findById(courseId)
-        // console.log(course);
-        
-        if(!course)
-            return NextResponse.json({success:false,message:"Course not found"},{status:404})
+        if (!courseId) {
+            return NextResponse.json({ success: false, message: 'Course ID is required' }, { status: 400 });
+        }
 
         const coursebyId = await Course.findById(courseId)
             .populate({
@@ -27,18 +18,27 @@ export const GET = async(req:NextRequest,res:NextResponse) => {
                 select: 'fullname email' 
             })
             .lean(); 
+        
+        // Use findOne instead of find to get a single document
+        const courseContent = await CourseContent.findOne({ course: courseId });
 
-        // if (course) {
-        //     // Add the enrolled students count to the course data
-        //     course.enrolledStudentCount = course.enrolledStudents.length;
-        //     // Remove the enrolledStudents array to keep only the count
-        //     delete course.enrolledStudents;
-        // }
+        // If courseContent exists, get its modules
+        const courseModules = courseContent ? courseContent.modules : [];
+        console.log("modules - ", courseModules);
 
-        return NextResponse.json({sucess:true,course:coursebyId},{status:200})
+        return NextResponse.json({
+            success: true, 
+            course: coursebyId, 
+            courseModules
+        }, { status: 200 });
+
     } catch (error) {
         console.log(error);
         
-        return NextResponse.json({success:false,message:"Something went wrong",error},{status:404})
+        return NextResponse.json({
+            success: false, 
+            message: "Something went wrong", 
+            error
+        }, { status: 404 });
     }
 }
